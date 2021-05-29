fef5fe375a9d5be7f5db4d2b7393c8656b24849e
Anoop Kumar <anoop.kumar@aventior.com>
2021-05-29 14:41:29 +0530

    updated with views

--- index.js
+++ index.js
@@ -2,6 +2,29 @@ const https = require('https');
 const fs = require('fs');
 const csvWriter = require('csv-write-stream');
 const sms = require('./testSmsApi');
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 5000
+
+let clearTrackerInterval;
+express()
+  .use(bodyParser.urlencoded({ extended: false }))
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index', { mobileNo: "HEY" }))
+  .post('/', (req, res) => {
+    if(clearTrackerInterval){
+      clearInterval(clearTrackerInterval);
+    }
+    vaccineTracker(req.body.mobileNo, req.body.feeLimit);
+    clearTrackerInterval=setInterval(()=>{vaccineTracker(req.body.mobileNo)}, 30000);;
+    return res.render('pages/index', { mobileNo: req.body.mobileNo ? req.body.mobileNo : "No mobileNo input 😥" });
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+
 
 const body = JSON.stringify({
   "sender_id": "TXTIND",
@@ -10,13 +33,13 @@ const body = JSON.stringify({
   "numbers": "9616202185"
 })
 
-function vaccineTracker() {
+function vaccineTracker(mobileNo, feeLimit) {
   console.log("working")
   const today = new Date();
   const date = today.toLocaleDateString('en-IN');
   https.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?Accept-Language=hi_IN&district_id=363&date=' + date, (resp) => {
     // https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?Accept-Language=hi_IN&district_id=363&date=26/05/2021  
-  let data = '';
+    let data = '';
 
     // A chunk of data has been received.
     resp.on('data', (chunk) => {
@@ -41,23 +64,23 @@ function vaccineTracker() {
                 AVAILABILIY_DATE: session["date"],
                 DATE: date,
                 TIME: today.toLocaleTimeString(),
-                FEE:session["fee"]
+                FEE: session["fee"]
               }
-              sms(vaccine_data, process.argv[2]);
+              sms(vaccine_data,mobileNo,feeLimit);
               writeToCSVFile(vaccine_data);
               console.log(`\t slots - ${session["available_capacity_dose1"]}\n pincode ${session["pincode"]}\n - date - ${session["date"]}\n\t  ${session["name"]}, ${session["address"]} on ${session["date"]}`);
             }
           })
         }
-      }catch (err) {
-  console.log(err);
-  console.log("Continuing");
-}
+      } catch (err) {
+        console.log(err);
+        console.log("Continuing");
+      }
     });
 
   }).on("error", (err) => {
-  //console.log("Error: " + err.message);
-});
+    //console.log("Error: " + err.message);
+  });
 };
 
 async function writeToCSVFile(vaccine_data) {
@@ -73,5 +96,3 @@ async function writeToCSVFile(vaccine_data) {
   writer.end();
 
 }
-vaccineTracker();
-setInterval(vaccineTracker, 30000);
